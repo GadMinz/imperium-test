ca7248b51078b0ef8286247e23bcdfa114704afa
GadMinz <baykov97@mail.ru>
2023-01-23 13:02:52 +0300

    Add best section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
 $(".navigation-open").click(() => {
-    $(".navigation").addClass("open");
-    $("body").addClass('lock')
+  $(".navigation").addClass("open");
+  $("body").addClass("lock");
 });
 
 $(window).click(function () {
-    $(".navigation").removeClass("open");
-    $("body").removeClass('lock')
+  $(".navigation").removeClass("open");
+  $("body").removeClass("lock");
 });
 
 $(".navigation").click(() => {
-    event.stopPropagation();
+  event.stopPropagation();
+});
+
+let bestSlider = new Swiper(".best-slider", {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  loop: true,
+  lazy: true,
+  pagination: {
+    el: ".best-slider-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".best-slider-next",
+    prevEl: ".best-slider-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    870: {
+      slidesPerView: 3,
+    },
+    1150: {
+      slidesPerView: 4,
+    },
+  },
 });
